0dccdb383db0c4dc29e12692b531aac6b9ef19c5
soroosh-gb <soroosh.gb@gmail.com>
2020-09-19 15:24:02 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,52 @@
 class BirdsController < ApplicationController
+
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    # dont need to set instance variables like @birds since we are not sending data to ERB files.
+    birds = Bird.all
+    render json: birds
   end
 end
+
+
+
+# or
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+#   end
+# end
+
+# or
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: @birds
+#   end
+# end
+
+
+
+# or
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+#   end
+# end
+
+# or
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { message: 'Hashes of data will get converted to JSON' }
+#   end
+# end
+
+# or
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: ['As','well','as','arrays']
+#   end
+# end
